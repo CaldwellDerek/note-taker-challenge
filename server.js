776de4ba56150991af516a4a7626f5b70c59d302
Derek Caldwell <djohncaldwell@gmail.com>
2023-01-31 19:21:53 -0800

    landing page and notes page routes added

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const server = express();
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const fs = require("fs");
+const generateUniqueId = require("generate-unique-id");
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true}));
+server.use(express.static("public"));
+
+// This sends the landing page to the client when requested from the server
+server.get("/", (request, response)=> {
+    response.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+// This sends the notes page to the client when requested from the server
+server.get("/notes", (request, response)=> {
+    response.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+// Message displayed to terminal when server is running
+server.listen(PORT, ()=> {
+    console.log(`SERVER RUNNING - Listening on port: ${PORT}`);
+});
