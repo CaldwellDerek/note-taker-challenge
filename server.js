a0a5575f0b91a3e66cfb1430a0540a2b6b0b512a
Derek Caldwell <djohncaldwell@gmail.com>
2023-01-31 19:25:37 -0800

    get route added for notes

--- server.js
+++ server.js
@@ -11,13 +11,24 @@ server.use(express.static("public"));
 
 // This sends the landing page to the client when requested from the server
 server.get("/", (request, response)=> {
-    response.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+    response.sendFile(path.join(__dirname, "/Develop/public/index.html"));
 });
 
 // This sends the notes page to the client when requested from the server
 server.get("/notes", (request, response)=> {
-    response.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+    response.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
 });
+
+server.get("/api/notes", (request, response)=> {
+    fs.readFile("./Develop/db/db.json", "utf-8", (error, data)=> {
+        if (error){
+            throw error;
+        } else {
+            response.json(JSON.parse(data));
+        }
+    })
+});
+
 
 // Message displayed to terminal when server is running
 server.listen(PORT, ()=> {
